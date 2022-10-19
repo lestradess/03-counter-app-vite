27f439ede359772ec59e68e7814b8a4aca7371d7
Lestrades_ubu <Lestrades@gmail.com>
2022-10-19 18:53:42 +0200

    Impresión de variables, funciones, etc.

--- src/FirsApp.jsx
+++ src/FirsApp.jsx
@@ -1,9 +1,17 @@
+const newMensaje = "Lestrades";
+
+const suma = (x)=> {
+    return x / 2;
+}
+
 export const FirsApp = () => {
+    
     return (
         <>
             <h2>FirsApp</h2>
             <h3>Agrupador</h3>
-            <p>Con el agrupador no se necesita poner divs</p>
+            <h4>Con el agrupador no se necesita poner divs</h4>
+            <p>{ suma(6)}</p>
         </>
         
     )
