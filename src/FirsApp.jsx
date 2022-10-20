8b3b7189b27c1fe3e601490fdafd855c40f25418
Lestrades_ubu <Lestrades@gmail.com>
2022-10-20 17:58:48 +0200

    Subida a GitHub

--- src/FirsApp.jsx
+++ src/FirsApp.jsx
@@ -2,14 +2,14 @@ const newMensaje = "Lestrades";
 
 
 
-export const FirsApp = ({ title,subtitle }) => {
-    
+export const FirsApp = ({ title, subtitle }) => {
+
     return (
         <>
             <h2>{title}</h2>
             <h3>{subtitle}</h3>
             <p>By Lestrades</p>
         </>
-        
+
     )
 }
