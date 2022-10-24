b840f1ce782f7712f0d3b9452d866714bf84d878
Lestrades_ubu <Lestrades@gmail.com>
2022-10-24 17:48:59 +0200

    Prueba toEqual 05-funciones

---
+++ tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,23 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/base-pruebas/05-funciones";
+describe('Pruebas en 05-funciones', () => {
+    test("getUser debe retornar un objeto", () => {
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+        const user = getUser();
+        console.log(user);
+
+        expect(testUser).toEqual(user);
+    })
+    test('getUsuario debe retornar un objeto', () => {
+        const name = 'Fernando';
+        const user = getUsuarioActivo(name);
+
+        expect(user).toEqual({
+            uid: "ABC567",
+            username: name
+        });
+
+    })
+});
