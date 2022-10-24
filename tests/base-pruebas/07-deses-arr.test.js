768af4e3821a53bba67dd8b16a4c20aa4a634136
Lestrades_ubu <Lestrades@gmail.com>
2022-10-24 18:06:02 +0200

    Pruebas toBE/toEqual  con typeof /expect.any

---
+++ tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,19 @@
+import { number } from "prop-types";
+import { retornaArreglo } from "../../src/base-pruebas/base-pruebas/07-deses-arr"
+
+describe('Pruebas en 07_deses-arr', () => {
+    test('Debe retornar un string y un número', () => {
+
+        const [ letras, numeros ] = retornaArreglo();
+        console.log(letras,numeros);
+
+        expect(typeof letras).toBe('string');
+        expect(typeof numeros).toBe("number");
+
+        expect(letras).toEqual(expect.any(String));
+        expect(numeros).toEqual(expect.any(Number))
+
+
+
+    })
+})
