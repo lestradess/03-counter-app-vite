163c65b6b2055cdeae1caf4296fe3aaa70a1d35e
Lestrades_ubu <Lestrades@gmail.com>
2022-10-24 16:47:07 +0200

    Autocompletado o intelligent para Jest

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,3 +1,12 @@
-test("Esta prueba no deve dar error", ()=> {
-    
+describe('Pruebas en Tal Componente', () => {
+    test("Esta prueba no deve dar error", () => {
+        // 1- inicialización
+        const message1 = "Hola mundo  ";
+        // 2- estímulo
+        const message2 = message1.trim();
+        // 3 Observar el comportamiento esperado
+        expect(message1).toBe(message2);
+
+    })
 })
+
