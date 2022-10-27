b673a973c154194a02fc15e12ccce3ce5941d95e
Lestrades_ubu <Lestrades@gmail.com>
2022-10-27 17:18:22 +0200

    finalizacion

--- tests/base-pruebas/001-FirsApp.test.jsx
+++ tests/base-pruebas/001-FirsApp.test.jsx
@@ -5,18 +5,18 @@ import { FirsApp } from "../../src/FirsApp"
 
 describe('Pruebas en FirsApp', () => {
 
-    test("debe quedar con las propiedades por defecto", () => {
-        const title = "Hola soy Goku";
-        const { container } = render(<FirsApp title={ title } />);
+    // test("debe quedar con las propiedades por defecto", () => {
+    //     const title = "Hola soy Goku";
+    //     const { container } = render(<FirsApp title={ title } />);
         
-        //Con esta funcion hace una instantanea de cómo esta la página
-        expect(container).toMatchSnapshot();
+    //     //Con esta funcion hace una instantanea de cómo esta la página
+    //     expect(container).toMatchSnapshot();
 
-    })
+    // })
     test ("debe mostrar el título en un h1", () => {
         const title = "Hola soy Goku";
         const { container,getByText } = render(<FirsApp title={ title } />);
-
+        // Evalua si existe el elemento title
         expect(getByText(title)).toBeTruthy();        
     })
 })
